2bd95dc424200ceccf42a7f5f1930d288f338381
Jordan Miracle <jordanmiracle@protonmail.com>
2021-08-03 04:31:34 -0700

    Adding in basic template files for strata layout

--- wanderingproject/settings.py
+++ wanderingproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityM/home/miracle/websites/wanderingwaters/frontend/staticiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- wanderingproject/urls.py
+++ wanderingproject/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from wanderingapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(''. views.index, name='index')
+    path('', views.index, name='index')
 ]
