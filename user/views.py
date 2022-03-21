15d7383da9a82df6293cac617fa95b87647402b8
Jordan Miracle <jordanmiracle@protonmail.com>
2022-03-20 20:00:11 -0700

    moved scripts in index and started creating forms and views for django users.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm, LoginForm
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username=username)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request, newUser)
+        messages.info(request, "Successfully registered..")
+
+        return redirect("index")
+    context = {
+        "form": form
+    }
+    return render(request, "register.html", context)
+
+
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+
+    context = {
+        "form": form
+    }
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            messages.info(request, "Username or password incorrect")
+            return render(request, "login.html", context)
+
+        messages.success(request, "Login Successful")
+        login(request, user)
+        return redirect("index")
+    return render(request, "login.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request, "Logout Successful")
+    return redirect("index")
