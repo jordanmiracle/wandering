aca3fff6114485bd61fa5fbf0ef80aa89c0c7bdf
Jordan Miracle <jordanmiracle@protonmail.com>
2022-03-28 03:54:35 -0700

    Added bootstrap to layout and login templates. Fixed some ID and names.
    Added views and requests for login page.

--- wanderingapp/views.py
+++ wanderingapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post, PostImage
 
 
 def blog_view(request):
     posts = Post.objects.all()
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, 'wanderingapp/index.html', {'posts': posts})
 
 
 def index(request):
@@ -16,3 +16,15 @@ def index(request):
         'post': post,
         'photos': photos
     })
+
+
+def addPhoto(request):
+    for image in PostImage:
+        photo = PostImage.objects.create(
+            image=image,
+        )
+
+    return redirect('gallery')
+
+
+return render(request, 'wanderingapp/add_photo.html', context)
