55e1340c38353bf99ae4d58b631e97dc60a7904b
Jordan Miracle <jordanmiracle@protonmail.com>
2022-05-28 04:11:08 -0700

    set allowed hosts

--- wanderingproject/settings.py
+++ wanderingproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = "*, mtbakerweddings.com, www.mtbakerweddings.com"
 
 # Application definition
 
