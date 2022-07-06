e259fe6c95f5a70b23dbc12dd8be6937b4a0edb4
jorda <jordanmiracle@protonmail.com>
2022-07-06 16:42:45 -0700

    Pushing views, forms, index, and new requirements to live branch. May need to add env variables to heroku

--- wanderingapp/forms.py
+++ wanderingapp/forms.py
@@ -3,10 +3,10 @@ from django.forms import ModelForm, Textarea, TextInput, EmailInput
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=200)
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea, max_length=2000)
+    name = forms.CharField(max_length=100, label='Name', required=False)
+    email = forms.CharField(max_length=200, label='Email', required=False)
+    subject = forms.CharField(max_length=100, label='Subject')
+    message = forms.CharField(widget=forms.Textarea, label='Message')
 
     def save(self):
         pass
--- wanderingapp/views.py
+++ wanderingapp/views.py
@@ -1,37 +1,67 @@
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponse
 from wanderingapp.forms import ContactForm
+#from wanderingproject.devsettings import EMAIL_HOST_USER
+#from wanderingproject.devsettings import AUTH_USER
 from .models import Slider, SliderImage
 from .forms import ContactForm
 from wanderingproject import settings
+from wanderingproject.settings import EMAIL_HOST_USER
 from django.conf import settings
+from wanderingproject import devsettings
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+
 
 def blog_view(request):
     posts = Slider.objects.all()
     return render(request, 'wanderingapp/index.html', {'posts': posts})
 
 
+# ef contact(request):
+#   if request.method == 'POST':
+#       form = ContactForm(request.POST or None)
+#       if form.is_valid():
+#           name = request.POST.get('name')
+#           subject = request.POST.get('subject')
+#           email = request.POST.get('email')
+#           message = request.POST.get('message')
+#           context = {
+#               'form': form,
+#           }
+#           return context
+
+#   form = ContactForm()
+#   return render(request, 'wanderingapp/index.html', {'form': form})
+
 def index(request):
     post = get_object_or_404(Slider)
     photos = SliderImage.objects.filter()
-    form_class = ContactForm()
+    submitted = False
     if request.method == 'POST':
-        form = ContactForm(request.POST or None)
+        form = ContactForm(request.POST)
         if form.is_valid():
-            name = request.POST.get('name')
-            subject = request.POST.get('subject')
-            email = request.POST.get('email')
-            message = request.POST.get('message')
-
-            send_mail('{}'.format(subject), '{}'.format(message), '{}'.format(email), [settings.EMAIL_HOST_USER],
-                      fail_silently=False)
-            return HttpResponseRedirect(ContactForm)
+            # assert false
+            con = get_connection('django.core.mail.backends.smtp.EmailBackend')
+            send_mail(
+                form.cleaned_data['subject'],
+                form.cleaned_data['message'],
+                form.cleaned_data['email'],
+                recipient_list=('ellen@mtbakerweddings.com',),
+                connection=con
+            )
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
     return render(request, 'wanderingapp/index.html', {
         'post': post,
         'photos': photos,
-        'form': form_class})
+        'form': form,
+        'submitted': submitted
+    })
 
 
 def coming_soon(request):
